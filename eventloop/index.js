7df31b153c12af1837fbbc10e98bd4466727f7dc
Charlie Abbott <chzabbott@gmail.com>
2023-09-11 00:58:46 +0900

    next steps

---
+++ eventloop/index.js
@@ -0,0 +1,16 @@
+
+process.nextTick(() => console.log('this is process.next 1'))
+process.nextTick(() => console.log('this is process.next 1'))
+
+
+
+
+// Promise.resolve().then(() => console.log('this is Promise.resolve 1'))
+// process.nextTick(() => console.log('this is process.next 1'))
+
+
+
+// console.log('console.log 1')
+// process.nextTick(() => console.log('this is process.next 1'))
+// console.log('console.log 2')
+
